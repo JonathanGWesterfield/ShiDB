fb51918319218ae2d082dbc8427ca1b1e487b04f
JonathanGWesterfield <jgwesterfield@gmail.com>
2026-02-09 14:50:58 -0500

    Added statistics functions to the file manager to see how many reads/writes/appends during program running

--- ShiDB-2.0/src/file/FileMgr.java
+++ ShiDB-2.0/src/file/FileMgr.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FileMgr {
     private File dbDirectory;
@@ -17,11 +19,36 @@ public class FileMgr {
     @Getter
     private boolean isNew;
 
+    // Can't autogenerate lombok getter function because it doesn't call AtomicInteger.get()
+    private AtomicInteger numBlocksWritten;
+    private AtomicInteger numBlocksRead;
+
+    @Getter
+    private ConcurrentHashMap<String, Integer> numFilesAppended;
+
     private Map<String, RandomAccessFile> openFiles = new HashMap<>();
+
+    public int getNumBlocksWritten() {
+        return numBlocksWritten.get();
+    }
+
+    public int getNumBlocksRead() {
+        return numBlocksRead.get();
+    }
+
+    public int getNumAppends(String filename) {
+        if (!numFilesAppended.containsKey(filename))
+            throw new RuntimeException("Filename " + filename + " hase no associated appends!");
+
+        return numFilesAppended.get(filename);
+    }
 
     public FileMgr(File dbDirectory, int blocksize) throws IOException {
         this.dbDirectory = dbDirectory;
         this.blocksize = blocksize;
+        this.numBlocksRead = new AtomicInteger(0);
+        this.numBlocksWritten = new AtomicInteger(0);
+        this.numFilesAppended = new ConcurrentHashMap<>();
 
         isNew = !dbDirectory.exists();
 
@@ -43,6 +70,8 @@ public class FileMgr {
             // This will read the contents of the file into the page bytebuffer by reference
             // They say Java doesn't have pointers, but references are basically the same thing
             accessFile.getChannel().read(page.getContents());
+
+            numBlocksRead.incrementAndGet();
         }
         catch (IOException e) {
             throw new RuntimeException("Cannot read block: " + block);
@@ -55,6 +84,8 @@ public class FileMgr {
             accessFile.seek(block.blockNum() * blocksize);
 
             accessFile.getChannel().write(page.getContents());
+
+            numBlocksWritten.incrementAndGet();
         }
         catch (IOException e) {
             throw new RuntimeException("Cannot write block: " + block);
@@ -71,6 +102,10 @@ public class FileMgr {
             RandomAccessFile accessFile = fetchFile(filename);
             accessFile.seek(newBlock.blockNum() * blocksize);
             accessFile.write(emptyByteArr);
+
+            numFilesAppended.putIfAbsent(filename, 0);
+            int numAppends = numFilesAppended.get(filename);
+            numFilesAppended.put(filename, ++numAppends);
         }
         catch (IOException e) {
             throw new RuntimeException("Cannot append block: " + newBlock);
@@ -89,14 +124,37 @@ public class FileMgr {
         }
     }
 
+    /**
+     * Fetches a file and returns the file handler. If no such file exists, one will be created and returned
+     * @param filename Name of the file to fetch/create
+     * @return accessFile File handler to use
+     * @throws IOException
+     */
     private RandomAccessFile fetchFile(String filename) throws IOException {
         RandomAccessFile accessFile = openFiles.get(filename);
 
         if (accessFile == null) {
             File dbTable = new File(dbDirectory, filename);
+
+            // RWS -> Read/Write/OS should not delay disk I/O to optimize disk performance
+            // S ensures that every write operation must be written immediately to the disk
             accessFile = new RandomAccessFile(dbTable, "rws");
             openFiles.put(filename, accessFile);
         }
         return accessFile;
+    }
+
+    public void deleteFile(String filename) {
+        try {
+            // First need to ensure that the random access file is closed
+            RandomAccessFile accessFile = fetchFile(filename);
+            accessFile.close();
+
+            new File(filename).delete();
+        }
+        catch (IOException e) {
+            // Since this function is purely used to help with unit testing, don't care if deletion fails
+            System.out.println("Failed to delete file: " + filename);
+        }
     }
 }
--- ShiDB-2.0/test/FileMgrTest.java
+++ ShiDB-2.0/test/FileMgrTest.java
@@ -6,6 +6,8 @@ import file.FileMgr;
 import file.BlockId;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -41,6 +43,74 @@ class FileMgrTest {
         assertEquals(page1.getString(position1), page2.getString(position1));
         assertEquals(page1.getInt(position2), page2.getInt(position2));
     }
+
+
+    @Test
+    @DisplayName("Check the number of writes to disk are being tracked correctly")
+    public void testFileWritesBeingTracked() {
+        BlockId blk = new BlockId("testfile", 2);
+        Page page1 = new Page(fileMgr.getBlocksize());
+
+        int position1 = 88;
+        String writeString = "The database name is a pun, isn't it?";
+
+        page1.setString(position1, writeString);
+
+        int numExpectedWrites = 4;
+        for (int i = 0; i < numExpectedWrites; i++)
+            fileMgr.writePageToDisk(blk, page1);
+
+        assertEquals(numExpectedWrites, fileMgr.getNumBlocksWritten());
+    }
+
+    @Test
+    @DisplayName("Check the number of reads from disk are being tracked correctly")
+    public void testFileReadsBeingTracked() {
+        BlockId blk = new BlockId("testfile", 2);
+        Page page1 = new Page(fileMgr.getBlocksize());
+
+        int position1 = 88;
+        String writeString = "The database name is a pun, isn't it?";
+
+        page1.setString(position1, writeString);
+
+        // Write to disk so we can read it
+        fileMgr.writePageToDisk(blk, page1);
+
+        int numExpectedReads = 4;
+        for (int i = 0; i < numExpectedReads; i++)
+            fileMgr.readFromDiskToPage(blk, page1);
+
+        assertEquals(numExpectedReads, fileMgr.getNumBlocksRead());
+    }
+
+    @Test
+    @DisplayName("Check that number of times various files are appended to")
+    public void checkNumFileAppends() {
+        String filename1 = "test1";
+        String fileName2 = "test2";
+        String fileName3 = "test3";
+
+        Map<String, Integer> numExpectedAppends = new HashMap<>();
+
+        // Setup how many times we want to append these files
+        // Want a different num of appends for each one to make obvious that this works or not
+        numExpectedAppends.put(filename1, 3);
+        numExpectedAppends.put(fileName2, 9);
+        numExpectedAppends.put(fileName3, 1);
+
+        // Setup the appends and check
+        for (Map.Entry<String, Integer> entry : numExpectedAppends.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                fileMgr.append(entry.getKey());
+            }
+
+            assertEquals(entry.getValue(), fileMgr.getNumAppends(entry.getKey()));
+        }
+        
+        // Cleanup
+        for (Map.Entry<String, Integer> entry: numExpectedAppends.entrySet())
+            fileMgr.deleteFile(entry.getKey());
+    }
+
 }
-
-
