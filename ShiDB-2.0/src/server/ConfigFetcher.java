09a8026692daaff0869a0505754c99ab2e2a5806
JonathanGWesterfield <jgwesterfield@gmail.com>
2026-02-11 21:00:06 -0500

    Added a config fetcher singleton to help manage configs so I'm not duplicating stuff everywhere

---
+++ ShiDB-2.0/src/server/ConfigFetcher.java
@@ -0,0 +1,60 @@
+package server;
+
+import lombok.Getter;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.core.JacksonException;
+
+import java.io.File;
+
+import java.nio.file.Paths;
+import java.util.Map;
+
+/**
+ * Singleton config fetcher to make setting up the database simpler. Things like buffer size, block size and
+ * other stuff will be in the config. This should make it much easier to create unit test setups since I only need
+ * to define things here and can likely override them as needed
+ */
+public class ConfigFetcher {
+    private static ConfigFetcher fetcherInstance;
+
+    @Getter
+    private Map<String, Object> configMap;
+
+    // On startup, need to read our config
+    private ConfigFetcher() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            File configFile = Paths.get("resources/config.json").toFile();
+
+            configMap = mapper.readValue(configFile, Map.class);
+        }
+        catch(JacksonException e) {
+            throw new RuntimeException("Failed to map the config file to Java Map! " + e.toString());
+        }
+    }
+
+    public static synchronized ConfigFetcher getConfigs() {
+        if (fetcherInstance == null)
+            fetcherInstance = new ConfigFetcher();
+
+        return fetcherInstance;
+    }
+
+    public static long getBufferMgrMaxWaitTime() {
+        if (getConfigs().configMap.containsKey("buffer_mgr_pin_max_wait_time_milliseconds"))
+            return Long.parseLong(getConfigs().configMap.get("buffer_mgr_pin_max_wait_time_milliseconds").toString());
+        return 10000L; // return a default wait time of 10 seconds
+    }
+
+    public static int getSizeOfBufferPool() {
+        if (getConfigs().configMap.containsKey("size_of_buffer_pool"))
+            return (int) getConfigs().configMap.get("size_of_buffer_pool");
+        return 3; // same default as used in the ShiDB unit tests
+    }
+
+    public static long getBufferMgrPollStepTime() {
+        if (getConfigs().configMap.containsKey("buffer_mgr_pin_poll_step_milliseconds"))
+            return Long.parseLong(getConfigs().configMap.get("buffer_mgr_pin_poll_step_milliseconds").toString());
+        return 100L; // return a default wait time of 100 milliseconds
+    }
+}
