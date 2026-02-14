64f02670efa2aa34b903566c0aca2511c5dd11e0
JonathanGWesterfield <jgwesterfield@gmail.com>
2026-02-13 21:36:33 -0500

    Fixed spelling mistakes and added dogma

--- ShiDB-2.0/src/main/java/buffer/Attempt.java
+++ ShiDB-2.0/src/main/java/buffer/Attempt.java
@@ -3,16 +3,18 @@ package buffer;
 /* This is an attempt at clean retry logic. I hate return null because:
  * 1. I hate null pointer exceptions
  * 2. I hate having to manually check for null every time
+ * 3. Checking for null doesn't actually tell me why something failed
  *
  * I don't want to use Optional because while it solves point 1, it just dresses up point 2 with lipstick. I still
- * have to manually check for null, just using the Optional class as a wrapper
+ * have to manually check for null, just using the Optional class as a wrapper. It also just leaves point 3
+ * completely unmolested. I'm sure I'm just ignant don't understand why Java programmers are ok with this
  *
  * I would like to use exceptions, but they really aren't meant for that and they are very computationally expensive
  * to generate the stack traces, so let's try creating a custom class to indicate hasSucceeded, or a need to retry
  *
  * This is very similar to the LeakyAbstractions Github library, (https://github.com/LeakyAbstractions/result)
  * but I didn't want to import another Jar for something this small (unless I need to use this in lots of places
- * in which case I'm might replace this)
+ * in which case I might replace this)
  */
 
 public record Attempt<T>(boolean hasSucceeded, T value) {
