76c3e1129a3539b369d20a0f9440b05cb8415867
JonathanGWesterfield <jgwesterfield@gmail.com>
2026-03-08 15:51:36 -0500

    Added new exception types to handle getting interrupted during a thread waiting

---
+++ ShiDB-2.0/src/main/java/error/CheckpointInterruptedException.java
@@ -0,0 +1,15 @@
+package error;
+
+public class CheckpointInterruptedException extends RuntimeException {
+    public CheckpointInterruptedException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public CheckpointInterruptedException(Throwable err) {
+        super(err);
+    }
+
+    public CheckpointInterruptedException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
+    }
+}
